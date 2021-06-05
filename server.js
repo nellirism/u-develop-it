497dfb1d465c2566e26ca9deb0853ce11c57ecd4
Cerebreo <nellirism@outlook.com>
2021-06-05 14:45:51 -0700

    queried the connected database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,6 +11,23 @@ app.use(express.json());
 app.use((req, res) => {
     res.status(404).end();
 });
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'TigerLily$1914',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});  
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
