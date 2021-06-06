b2a6d97498bd2c90b8967e2de4b2c344fe643670
Cerebreo <nellirism@outlook.com>
2021-06-05 17:18:37 -0700

    modularized db and routes code and addig routes to the route files created

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const { moduleExpression } = require('@babel/types');
+const mysql = require('mysql2');
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'TigerLily$1914',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
